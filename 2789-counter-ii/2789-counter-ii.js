be06d3e57ddae7e4d960093d9cf8e77bc0acea77
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2025-11-13 18:45:18 +0700

    Time: 46 ms (85.83%), Space: 56.2 MB (70.86%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    counter = init;
+    function increment(){
+        return ++counter;
+    }
+
+    function decrement(){
+         return --counter;
+    }
+
+    function reset(){
+        counter = init;
+         return counter;
+    }
+
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset,
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
