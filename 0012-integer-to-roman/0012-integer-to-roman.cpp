11c2ca07eb0ecefc76dc424a6f10de6e203e8863
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-09 21:01:05 +0700

    Time: 6 ms (32.95%), Space: 12.7 MB (31.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        // Roman symbols and their values (descending order)
+        vector<pair<int, string>> roman = {
+            {1000, "M"},
+            {900,  "CM"},
+            {500,  "D"},
+            {400,  "CD"},
+            {100,  "C"},
+            {90,   "XC"},
+            {50,   "L"},
+            {40,   "XL"},
+            {10,   "X"},
+            {9,    "IX"},
+            {5,    "V"},
+            {4,    "IV"},
+            {1,    "I"}
+        };
+
+        string result = "";
+
+        for(int i = 0; i < roman.size(); i++){
+            int value = roman[i].first;
+            string symbol = roman[i].second;
+
+            while(num >= value){
+                result += symbol;
+                num -= value;
+            }
+        }
+
+        return result;
+    }
+};
