d55d9478cde58096d4fe90ac31bb9384685c74c8
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-11 20:06:28 +0700

    Time: 0 ms (100%), Space: 28.4 MB (63.99%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-       unordered_set<int> hs;
-        for(int n : nums){
-            if(hs.count(n)){
-                return n;
-            }
-            hs.insert(n);
+     int n = nums.size();
+     for( int i = 0; i < n - 2; i++){
+        if(nums[i] == nums[i + 1] || nums[i] == nums[i + 2]){
+            return nums[i];
         }
-       return -1;
+     }
+      return nums[n-1];
     }
 };
