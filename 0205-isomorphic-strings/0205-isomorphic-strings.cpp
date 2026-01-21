587a49d6a3642b90533b6920f57cd63dd1a7ff77
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-21 20:35:03 +0700

    Time: 4 ms (23.07%), Space: 9.4 MB (55.58%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, char> mp1; 
+        unordered_map<char, char> mp2;
+
+        for (int i = 0; i < s.size(); i++) {
+            char a = s[i], b = t[i];
+
+            if (mp1.count(a) && mp1[a] != b) return false;
+            if (mp2.count(b) && mp2[b] != a) return false;
+
+            mp1[a] = b;
+            mp2[b] = a;
+        }
+        return true;
+    }
+};
