84048d5afeba8d5abf7cc5b82a8638809af804e6
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-25 20:42:26 +0700

    Time: 0 ms (100%), Space: 17.1 MB (84.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = prices[0];
-        int totalProfit = 0;
-    
+        int profit = 0;
+
         for(int i = 1; i < prices.size(); i++){
-            if(prices[i] < prices[i-1]){
-                totalProfit += prices[i-1] - minPrice;
-                minPrice = prices[i];
-            }
+            if(prices[i] > prices[i-1])
+                profit += prices[i] - prices[i-1];
         }
-        totalProfit += prices.back() - minPrice;
-        return totalProfit;
+
+        return profit;
     }
 };
