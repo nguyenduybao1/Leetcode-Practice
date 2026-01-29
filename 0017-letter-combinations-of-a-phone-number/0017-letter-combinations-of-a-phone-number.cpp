5105d0b0b4f1f0ca0d96145f2b3d48192d8a1308
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-29 20:48:34 +0700

    Time: 0 ms (100%), Space: 9 MB (94.7%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char, string> mp = {
+            {'2', "abc"}, {'3', "def"},
+            {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
+        };
+
+        vector<string> res;
+        string path;
+
+        backtrack(0, digits, mp, path, res);
+        return res;
+    }
+private:
+    void backtrack(int idx, string& digits, 
+                   unordered_map<char, string>& mp, 
+                   string& path, vector<string>& res){
+        if(idx == digits.size()){
+            res.push_back(path);
+            return;
+        }
+
+        for(char c : mp[digits[idx]]){
+            path.push_back(c);
+            backtrack(idx + 1, digits, mp, path, res);
+            path.pop_back();
+        }
+    }
+};
