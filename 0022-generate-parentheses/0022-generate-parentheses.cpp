4f699d7b1be4708d20d0443a733002a41afa7358
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-29 21:31:28 +0700

    Time: 5 ms (18.12%), Space: 16.1 MB (27.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> res;
+    void backtrack(string path, int open, int close, int n){
+        if(path.size() == 2 * n){
+            res.push_back(path);
+            return;
+        }
+
+        if(open < n){
+            backtrack(path + "(", open + 1, close, n);
+        }
+
+        
+        if(close < open){
+            backtrack(path + ")", open, close + 1, n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+      backtrack("", 0, 0, n);
+      return res;
+    }
+};
