8ac4b633a595904894c69b86c7fff7a9971f072c
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-01-30 20:38:17 +0700

    Time: 8 ms (71.95%), Space: 9.4 MB (78.92%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if(n < 2) return s;
+        
+        int start = 0, maxLen = 1;
+        for(int i = 0; i < s.size(); i++){
+            expand(s, i, i, start, maxLen);
+            expand(s, i, i+1, start, maxLen);
+        }
+        return s.substr(start, maxLen);
+    }
+
+    void expand(string& s, int left, int right, int& start, int& maxLen){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        int len = right - left - 1;
+
+        if(len > maxLen){
+            maxLen = len;
+            start = left + 1;
+        }
+    }
+};
