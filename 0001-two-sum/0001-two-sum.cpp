7d5708573f0f09acb9b0dbccdf4d30003bf11ab9
Nguyễn Duy Bảo - Software Engineer <100182329+nguyenduybao1@users.noreply.github.com>
2026-02-02 22:33:33 +0700

    Time: 1 ms (76.96%), Space: 15 MB (19%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size(); i++){
             int cur = nums[i];
-            int x = target - cur;
-
-            if (mp.count(x)) {
-                return {mp[x], i};
+            int ans = target - cur;
+            if(mp.count(ans)){
+                return {mp[ans], i};
             }
-
             mp[cur] = i;
         }
-
         return {};
     }
 };
