9c48e0925954a3808d53c524e8dd68921509be92
Bảo Nguyễn <100182329+nguyenduybao1@users.noreply.github.com>
2026-02-12 20:42:59 +0700

    Time: 35 ms (98.37%), Space: 11.5 MB (94.41%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int longestBalanced(string s) {
         int n = s.size();
-        if(n < 2) return n;
-
         int ans = 1;
-        for(int left = 0; left < n; left++){
-            unordered_map<char, int> mp;
-            for(int right = left; right < n; right++){
-                mp[s[right]]++;
-                
-                int val = -1;
-                bool ok = true;
-                for(auto &p : mp){
-                    if(val == -1){
-                        val = p.second;
-                    }else if(p.second != val){
-                        ok = false;
-                        break;
-                    }
+
+        for (int left = 0; left < n; left++) {
+            int freq[26] = {0};
+            int distinct = 0;
+            int maxFreq = 0;
+            int cntMax = 0;
+
+            for (int right = left; right < n; right++) {
+                int c = s[right] - 'a';
+                int f = ++freq[c];
+
+                if (f == 1) distinct++;
+
+                if (f > maxFreq) {
+                    maxFreq = f;
+                    cntMax = 1;
+                } else if (f == maxFreq) {
+                    cntMax++;
                 }
 
-                if(ok){
+                if (distinct == cntMax) {
                     ans = max(ans, right - left + 1);
                 }
             }
